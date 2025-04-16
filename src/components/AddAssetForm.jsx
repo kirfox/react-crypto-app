afbf7c187fef27d45518c4e443fcc560875ca705
kmukhin <kmukhin@phoenixit.ru>
2025-04-16 17:31:57 +0300

    made selector for header

---
+++ src/components/AddAssetForm.jsx
@@ -0,0 +1,47 @@
+import {Divider, Flex, Select, Space, Typography } from "antd";
+import { useState } from "react"
+import { useCrypto } from "../context/crypto-context";
+
+export default function AddAssetForm() {
+    const {crypto} = useCrypto()
+    const [coin, setCoin] = useState(null)
+
+    if(!coin) {
+        return(
+            <Select
+                
+                style={{ width: '100%' }}
+                placeholder="Select coin"
+                onSelect={(v)=> setCoin(crypto.find( c => c.id === v))}
+                options={crypto.map((coin) => ({
+                label: coin.name,
+                value: coin.id,
+                icon: coin.icon,
+                }))}
+                optionRender={(option) => (
+                <Space>
+                    <img
+                    style={{ width: 20 }}
+                    src={option.data.icon}
+                    alt={option.data.label}
+                    />{" "}
+                    {option.data.label}
+                </Space>
+            )}
+            />
+        )
+    }
+    return <form>
+        <Flex align="center">
+            <img
+                src={coin.icon}
+                alt={coin.name}
+                style={{ width: 40, marginRight: 10 }}
+            />
+            <Typography.Title level={2} style={{ margin: 0 }}>
+                {coin.name}
+            </Typography.Title>
+        </Flex>
+        <Divider></Divider>
+    </form>
+}
