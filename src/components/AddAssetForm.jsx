877b5cd3a6168eabeea3e3015b4a2d54355c4de7
kirfox <muxin.kirill2011@yandex.ru>
2025-04-17 18:33:26 +0300

    made assetFom for header

--- src/components/AddAssetForm.jsx
+++ src/components/AddAssetForm.jsx
@@ -1,47 +1,147 @@
-import {Divider, Flex, Select, Space, Typography } from "antd";
-import { useState } from "react"
+import {
+  Divider,
+  Flex,
+  Select,
+  Space,
+  Typography,
+  Form,
+  Button,
+  InputNumber,
+  DatePicker,
+  Result,
+} from "antd";
+import { useRef, useState } from "react";
 import { useCrypto } from "../context/crypto-context";
+import CoinInfo from "./CoinInfo";
 
-export default function AddAssetForm() {
-    const {crypto} = useCrypto()
-    const [coin, setCoin] = useState(null)
+const validateMassages = {
+  required: "${label} us required!",
+  types: {
+    number: "${label} is not valid number",
+  },
+  number: {
+    range: "${label} must be between ${min} and ${max}",
+  },
+};
 
-    if(!coin) {
-        return(
-            <Select
-                
-                style={{ width: '100%' }}
-                placeholder="Select coin"
-                onSelect={(v)=> setCoin(crypto.find( c => c.id === v))}
-                options={crypto.map((coin) => ({
-                label: coin.name,
-                value: coin.id,
-                icon: coin.icon,
-                }))}
-                optionRender={(option) => (
-                <Space>
-                    <img
-                    style={{ width: 20 }}
-                    src={option.data.icon}
-                    alt={option.data.label}
-                    />{" "}
-                    {option.data.label}
-                </Space>
-            )}
-            />
-        )
-    }
-    return <form>
-        <Flex align="center">
+export default function AddAssetForm({ onClose }) {
+  const [form] = Form.useForm();
+  const { crypto, addAsset } = useCrypto();
+  const [coin, setCoin] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+  const assetRef = useRef();
+
+  if (submitted) {
+    return (
+      <Result
+        status="success"
+        title="New Asset Added"
+        subTitle={`Added ${assetRef.current.amount} of ${coin.name} by price ${assetRef.current.price}`}
+        extra={[
+          <Button type="primary" key="console" onClick={onClose}>
+            Close
+          </Button>,
+        ]}
+      />
+    );
+  }
+  if (!coin) {
+    return (
+      <Select
+        style={{ width: "100%" }}
+        placeholder="Select coin"
+        onSelect={(v) => setCoin(crypto.find((c) => c.id === v))}
+        options={crypto.map((coin) => ({
+          label: coin.name,
+          value: coin.id,
+          icon: coin.icon,
+        }))}
+        optionRender={(option) => (
+          <Space>
             <img
-                src={coin.icon}
-                alt={coin.name}
-                style={{ width: 40, marginRight: 10 }}
-            />
-            <Typography.Title level={2} style={{ margin: 0 }}>
-                {coin.name}
-            </Typography.Title>
-        </Flex>
-        <Divider></Divider>
-    </form>
+              style={{ width: 20 }}
+              src={option.data.icon}
+              alt={option.data.label}
+            />{" "}
+            {option.data.label}
+          </Space>
+        )}
+      />
+    );
+  }
+
+  function onFinish(values) {
+    const newAsset = {
+      id: coin.id,
+      amount: values.amount,
+      price: values.price,
+      date: values.date?.$d ?? new Date(),
+    };
+    assetRef.current = newAsset;
+
+    setSubmitted(true);
+    addAsset(newAsset);
+  }
+
+  function handleAmountChange(value) {
+    const price = form.getFieldValue("price");
+    form.setFieldsValue({
+      total: +(value * price).toFixed(2),
+    });
+  }
+
+  function handlePriceChange(value) {
+    const amount = form.getFieldValue("amount");
+    form.setFieldsValue({
+      total: +(amount * value).toFixed(2),
+    });
+  }
+
+  return (
+    <Form
+      form={form}
+      name="basic"
+      labelCol={{ span: 4 }}
+      wrapperCol={{ span: 10 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{
+        price: +coin.price.toFixed(2),
+      }}
+      onFinish={onFinish}
+      validateMassage={validateMassages}
+    >
+      <CoinInfo coin={coin}></CoinInfo>
+      <Divider></Divider>
+
+      <Form.Item
+        label="Amount"
+        name="amount"
+        rules={[{ required: true, type: "number", min: 0 }]}
+      >
+        <InputNumber
+          placeholder="Entter coin amount"
+          style={{ width: "100%" }}
+          onChange={handleAmountChange}
+        />
+      </Form.Item>
+
+      <Form.Item label="Price" name="price">
+        <InputNumber onChange={handlePriceChange} style={{ width: "100%" }} />
+      </Form.Item>
+
+      <Form.Item label="Date & Time" name="date">
+        <DatePicker showTime />
+      </Form.Item>
+
+      <Form.Item label="Total" name="total">
+        <InputNumber disabled style={{ width: "100%" }} />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Add Asset
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 }
